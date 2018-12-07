1ede2329d2795f5c502ce46b8015076886f160ee
xixiaby <1341653837@qq.com>
2018-12-07 17:18:20 +0800

    第五次作业

---
+++ app/src/main/java/com/example/baiyang/myapplication/Main2Activity.java
@@ -0,0 +1,25 @@
+package com.example.baiyang.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class Main2Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        ImageButton imageButton=(ImageButton)findViewById(R.id.imageButton_back);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Main2Activity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/baiyang/myapplication/Main3Activity.java
@@ -0,0 +1,24 @@
+package com.example.baiyang.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        ImageButton imageButton=(ImageButton)findViewById(R.id.imageButton_back);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Main3Activity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
